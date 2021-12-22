6e297d7e3198360c3e92778cc76122ac77342c63
MAdnanNaeem <madnannaeem@hotmail.com>
2021-12-22 17:59:27 +0500

    commit 1

--- 11. Student Registration Form/script.js
+++ 11. Student Registration Form/script.js
@@ -21,11 +21,12 @@ function alpha_positive_negative(event) {
         event.charCode == 45;
 }
 
-// ! Height
+// ! Height & Weight
 
 function validate_hg_wg(event) {
   return (event.which === 32 && event.target.selectionStart === 0) ||
-    (event.which === 46 && event.target.selectionStart === 0)
+    (event.which === 46 && event.target.selectionStart === 0) ||
+    (event.which === 48 && event.target.selectionStart === 0)
     ? false
     : (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46;
 }
@@ -37,4 +38,31 @@ function valid_idcard(event) {
     (event.which === 45 && event.target.selectionStart === 0)
     ? false
     : (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 45;
+}
+
+// ! Phone No
+
+function validate_phno(event) {
+  return event.which === 32 && event.target.selectionStart === 0
+    ? false
+    : (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 43;
+}
+
+// ! Major Subjects & Address
+function validation_Edu_addr(event) {
+  return (event.which === 32 && event.target.selectionStart === 0) ||
+    (event.which === 44 && event.target.selectionStart === 0)
+    ? false
+    : (event.charCode >= 65 && event.charCode <= 90) ||
+        (event.charCode >= 97 && event.charCode <= 122) ||
+        event.charCode == 32 ||
+        event.charCode == 44;
+}
+
+// ! Simple digits
+
+function validate_numb(event) {
+  return event.which === 32 && event.target.selectionStart === 0
+    ? false
+    : event.charCode >= 48 && event.charCode <= 57;
 }
